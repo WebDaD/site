3504b6a0adab3206226917e726bdae979b72f7a6
Domink Sigmund <sigmund.dominik@googlemail.com>
2017-06-02 09:19:30 +0200

    title="Share on Google+"

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,8 +18,9 @@ $(document).ready(function () {
   $("a[href^='#']").on('click', function (e) {
     e.preventDefault()
     var haash = this.hash
+    var paddingTop = parseInt($('body').css('padding-top').replace('px', '')) + 20
     $('html, body').animate({
-      scrollTop: $(haash).offset().top - 70
+      scrollTop: $(haash).offset().top - paddingTop
     }, 300, function () {
       window.location.hash = haash
     })
